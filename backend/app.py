4abd0185d48abefbcd41ac80d615b3176d9c7a23
Kirtan011 <kirtansuthar1112@gmail.com>
2025-08-12 22:29:26 +0530

    Remove Development Port Setting

--- backend/app.py
+++ backend/app.py
@@ -41,9 +41,7 @@ def upload_and_transcribe():
         return jsonify({"success": False, "error": str(e)}), 500
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)
+
 
 
 
